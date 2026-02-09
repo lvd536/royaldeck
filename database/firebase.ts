27513b768b963a8389edb4f91175041074df6cd7
lvd536 <nbikiti@gmail.com>
2026-02-09 21:56:48 +0400

    - Add new packages & create env example

---
+++ database/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFf0hfpxayei1huPdpEisPQEarK98Y9K8",
+  authDomain: "royaldeck-7d63a.firebaseapp.com",
+  projectId: "royaldeck-7d63a",
+  storageBucket: "royaldeck-7d63a.firebasestorage.app",
+  messagingSenderId: "518569059175",
+  appId: "1:518569059175:web:6f67308afb3339125c85c2",
+  measurementId: "G-1ENRL1PZDR"
+};
+
+const app = initializeApp(firebaseConfig);
