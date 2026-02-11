8d523ef7455ca61a1fc894083ae1f1a603d139b3
lvd536 <nbikiti@gmail.com>
2026-02-12 01:30:17 +0400

    - Add avg elixir & cycle elixir into deck builder + image sizing edits

--- components/Pages/DeckCreation.tsx
+++ components/Pages/DeckCreation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CardResponse } from "@varandas/clash-royale-api/lib/interfaces";
+import { CardResponse, ICard } from "@varandas/clash-royale-api/lib/interfaces";
 import { useState } from "react";
 import emptyCard from "@/public/empyCard.png";
 import Image from "next/image";
@@ -23,7 +23,7 @@ const initialDeck: ICustomDeck = {
     name: "Deck 0",
     elixir: 0,
     cycle: 0,
-    cards: { 0: "", 1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: "" },
+    cards: { 0: "", 1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "" },
 };
 
 export default function DeckCreation({ cards }: IProps) {
@@ -70,6 +70,49 @@ export default function DeckCreation({ cards }: IProps) {
         closeCardList();
     };
 
+    const calcDeckElixir = (deckCards: Record<number, string>) => {
+        return (
+            Object.values(deckCards).reduce((sum, value) => {
+                const card = cards.items.find(
+                    (i) =>
+                        i.iconUrls.medium === value ||
+                        i.iconUrls.evolutionMedium === value,
+                );
+                return sum + (Number(card?.elixirCost) || 0);
+            }, 0) / 8
+        ).toFixed(1);
+    };
+
+    const calcCycleElixir = (deckCards: Record<number, string>) => {
+        return getLowCostCards(deckCards).reduce((acc, [key, url]) => {
+            const cardInfo = cards.items.find(
+                (i) =>
+                    i.iconUrls.medium === url ||
+                    i.iconUrls.evolutionMedium === url,
+            );
+            const cost = cardInfo?.elixirCost || 0;
+
+            return acc + cost;
+        }, 0);
+    };
+
+    const getLowCostCards = (deckCards: Record<number, string>) => {
+        const costMap = new Map(
+            cards.items.flatMap((i) => [
+                [i.iconUrls.medium, i.elixirCost],
+                [i.iconUrls.evolutionMedium, i.elixirCost],
+            ]),
+        );
+
+        return Object.entries(deckCards)
+            .sort(([, urlA], [, urlB]) => {
+                const costA = costMap.get(urlA) || 99;
+                const costB = costMap.get(urlB) || 99;
+                return costA - costB;
+            })
+            .slice(0, 4);
+    };
+
     return (
         <div className="w-full flex flex-col max-md:px-2 mt-5">
             <ul className="w-full flex flex-col items-center justify-center mx-auto">
@@ -103,9 +146,9 @@ export default function DeckCreation({ cards }: IProps) {
                                                 <Image
                                                     src={src}
                                                     alt={`card-${slotIndex}`}
-                                                    width={100}
-                                                    height={150}
-                                                    className="object-fit mb-2.5"
+                                                    width={500}
+                                                    height={1000}
+                                                    className="w-[102px] h-auto object-fit mb-3"
                                                 />
                                             ) : (
                                                 <Image
@@ -113,6 +156,8 @@ export default function DeckCreation({ cards }: IProps) {
                                                     width={110}
                                                     height={160}
                                                     alt="empty"
+                                                    className="w-[110px] h-auto"
+                                                    loading="eager"
                                                 />
                                             )}
                                         </li>
@@ -127,8 +172,9 @@ export default function DeckCreation({ cards }: IProps) {
                                     alt="elixir icon"
                                     width={35}
                                     height={35}
+                                    className="w-10 h-auto"
                                 />
-                                <p>{deck.elixir}</p>
+                                <p>{calcDeckElixir(deck.cards)}</p>
                             </div>
                             <div className="flex items-center font-clash-regular text-sm gap-2">
                                 <Image
@@ -136,8 +182,9 @@ export default function DeckCreation({ cards }: IProps) {
                                     alt="cycle icon"
                                     width={25}
                                     height={25}
+                                    className="w-5 h-auto"
                                 />
-                                <p>{deck.cycle}</p>
+                                <p>{calcCycleElixir(deck.cards)}</p>
                             </div>
                         </div>
                     </li>
@@ -174,9 +221,10 @@ export default function DeckCreation({ cards }: IProps) {
                                     <Image
                                         src={i.iconUrls.medium}
                                         alt={`card-${i.id}`}
-                                        width={50}
-                                        height={100}
+                                        width={500}
+                                        height={1000}
                                         style={{ cursor: "pointer" }}
+                                        className="w-20 h-auto"
                                         onClick={() =>
                                             handleAddCard(i.iconUrls.medium)
                                         }
@@ -185,9 +233,10 @@ export default function DeckCreation({ cards }: IProps) {
                                         <Image
                                             src={i.iconUrls.evolutionMedium}
                                             alt={`card-evo-${i.id}`}
-                                            width={50}
-                                            height={100}
+                                            width={500}
+                                            height={1000}
                                             style={{ cursor: "pointer" }}
+                                            className="w-20 h-auto"
                                             onClick={() =>
                                                 handleAddCard(
                                                     i.iconUrls.evolutionMedium!,
